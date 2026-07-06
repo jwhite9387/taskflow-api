dd52a9b211bd319a7f54004e9164d5e2af116745
jwhite9387 <jwhite93@uab.edu>
2026-07-06 00:29:00 -0500

    Added user validation to user endpoint

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -15,6 +15,28 @@ type RegisterUserResponse struct {
 	Message string `json:"message"`
 }
 
+type ValidationErrorResponse struct {
+	Errors []string `json:"errors"`
+}
+
+func (r RegisterUserRequest) Validate() []string {
+	var errs []string
+	if r.Username == "" {
+		errs = append(errs, "Username is required")
+	}
+
+	if r.Email == "" {
+		errs = append(errs, "Email is required")
+	}
+
+	if r.Password == "" {
+		errs = append(errs, "Password is required")
+	} else if len(r.Password) < 8 {
+		errs = append(errs, "Password must be at least 8 characters")
+	}
+	return errs
+}
+
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
@@ -25,6 +47,18 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	var user RegisterUserRequest
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if errs := user.Validate(); len(errs) > 0 {
+		response := ValidationErrorResponse{
+			Errors: errs,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		if err := json.NewEncoder(w).Encode(response); err != nil {
+			// TODO: Log the error when we add logging
+		}
 		return
 	}
 
